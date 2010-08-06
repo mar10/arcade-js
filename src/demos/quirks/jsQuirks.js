8b42718f2c32213fb5d68459671b387aef7caefb
Martin <Martin@LT>
2010-08-06 12:56:42 +0200

    Added Quirks sample

---
+++ src/demos/quirks/jsQuirks.js
@@ -0,0 +1,74 @@
+/**
+ * jsQuirks.js
+ * 
+ */
+
+
+var QuirksGame = ArcadeJS.extend({
+    init: function(canvas, customOpts) {
+		// Init ArcadeJS
+		var opts = $.extend({
+			name: "jsQuirks",
+			fps: 30
+		}, customOpts);
+        this._super(canvas, opts);
+        
+        // Set the scene
+        this.addObject(new Quirk());
+        // Start render loop
+        this.startLoop()
+    },
+    // --- end of class
+    lastentry: undefined
+});
+
+
+/******************************************************************************/
+
+
+var Quirk = Movable.extend({
+    init: function(opts) {
+		opts = $.extend({
+		}, opts);
+        this._super("quirk", null, opts);
+        this.maxLines = 20;
+        this.lines = [];
+        this.pos1 = new Point2(LinaJS.randomInt(100, 200), LinaJS.randomInt(100, 200));
+        this.move1 = new Vec2(LinaJS.randomInt(1, 5), LinaJS.randomInt(1, 5));
+        this.pos2 = new Point2(LinaJS.randomInt(100, 200), LinaJS.randomInt(100, 200));
+        this.move2 = new Vec2(LinaJS.randomInt(1, 5), LinaJS.randomInt(1, 5));
+    },
+    step: function() {
+    	// Add another line (discard oldest, if max. is reached)
+    	// Note that we use pos.copy() so every line has it's own Point2 instance
+    	if( this.lines.length > this.maxLines )
+    		this.lines.shift();
+    	this.lines.push({pos1: this.pos1.copy(), pos2: this.pos2.copy()});
+    	// Calculate new position for first point
+    	this.pos1.translate(this.move1);
+    	// Invert velocity vector, when bouncing at the canvas borders
+    	if(this.pos1.x < 0 || this.pos1.x >= this.game.canvas.width)
+    		this.move1.dx *= -1;
+    	if(this.pos1.y < 0 || this.pos1.y >= this.game.canvas.height)
+    		this.move1.dy *= -1;
+    	// Calculate new position and velocity for second point
+    	this.pos2.translate(this.move2);
+    	if(this.pos2.x < 0 || this.pos2.x >= this.game.canvas.width)
+    		this.move2.dx *= -1;
+    	if(this.pos2.y < 0 || this.pos2.y >= this.game.canvas.height)
+    		this.move2.dy *= -1;
+    },
+    render: function(ctx) {
+    	// Draw the list of lines to the canvas
+    	ctx.strokeStyle = "#80ff80";
+		for(var i=0; i<this.lines.length; i++){
+			var l = this.lines[i];
+			ctx.beginPath();  
+			ctx.moveTo(l.pos1.x, l.pos1.y);  
+			ctx.lineTo(l.pos2.x, l.pos2.y);
+			ctx.stroke();
+		} 
+	},
+    // --- end of class
+    lastentry: undefined
+});

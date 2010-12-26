198f46ac4f37ce0449cee98a54f295dff1d5439b
Martin <Martin@Athlon5000>
2010-12-26 09:47:07 +0100

    Added obj.later()

--- src/arcade.js/test/test-arcade.js
+++ src/arcade.js/test/test-arcade.js
@@ -3,50 +3,50 @@ $(function(){
 	 * Tool functions
 	 */
 	function makeBenchWrapper(testName, callback) {
-	    return function() {
-	        var start = +new Date;
+		return function() {
+			var start = +new Date;
 //	        callback.apply(this, arguments);
-	        callback.call();
-	        var elap = +new Date - start;
-	        ok(true, testName + " took " + elap + " milliseconds");
-	    }
+			callback.call();
+			var elap = +new Date - start;
+			ok(true, testName + " took " + elap + " milliseconds");
+		}
 	}
 
 
 	function benchmark(testName, callback) {
-	    // Execute callback immediately and log timing as test result.
-	    // This function should be called inside a test() function.
-	    makeBenchWrapper(testName, callback).call();
+		// Execute callback immediately and log timing as test result.
+		// This function should be called inside a test() function.
+		makeBenchWrapper(testName, callback).call();
 	}
 
 
 	function timedTest(testName, callback) {
-	    // Same as test(testName, callback), but adds a timing assertion.
-	    test(testName, makeBenchWrapper(testName, callback));
+		// Same as test(testName, callback), but adds a timing assertion.
+		test(testName, makeBenchWrapper(testName, callback));
 	}
 
 
 	function simulateClick(selector) {
-	    var e = document.createEvent("MouseEvents");
-	    e.initEvent("click", true, true);
-	    $(selector).each(function(){
-	        this.dispatchEvent(e);
-	    });
+		var e = document.createEvent("MouseEvents");
+		e.initEvent("click", true, true);
+		$(selector).each(function(){
+			this.dispatchEvent(e);
+		});
 	};
 
 /*******************************************************************************
  * QUnit setup
  */
-QUnit.log = function(result, message) {  
-  if (window.console && window.console.log) {  
-      window.console.log(result +' :: '+ message);  
-  }  
-}  
+QUnit.log = function(result, message) {
+  if (window.console && window.console.log) {
+	  window.console.log(result +' :: '+ message);
+  }
+}
 
 function logMsg(){
-    if (window.console && window.console.log) {  
-        window.console.log.apply(this, arguments);  
-    }  
+	if (window.console && window.console.log) {
+		window.console.log.apply(this, arguments);
+	}
 }
 
 
@@ -56,10 +56,10 @@ function logMsg(){
 module("Vector math");
 
 test("basic", function() {
-    expect(2);
+	expect(2);
 
 	var game = new ArcadeJS();
-    ok(game.fps == 15, "Construct ArcadeJS");
+	ok(game.fps == 15, "Construct ArcadeJS");
 });
 
 
